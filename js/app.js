e35de9ba258627423ee23febba55d87a5cb78513
Fahim-H <fahimlutful@gmail.com>
2022-09-03 00:51:32 +0600

    draft modal

--- js/app.js
+++ js/app.js
@@ -61,6 +61,8 @@ const displayNewsByCategory = news => {
     if (item.details.length > 400) {
       itemDetail = item.details.slice(0, 400);
       itemDetail += '...';
+    } else {
+      itemDetail = item.details;
     }
 
     const newsElement = document.createElement('div');
@@ -68,7 +70,7 @@ const displayNewsByCategory = news => {
       <div id="item-container " class="flex space-x-10">
         <div class="w-1/4">
           <img
-            class="object-cover h-full"
+            class="object-cover h-full p-5 rounded-3xl"
             src="${item.image_url}"
             alt=""
           />
@@ -90,16 +92,52 @@ const displayNewsByCategory = news => {
                 src="${item.author.img}"
                 alt=""
               />
-              <h4>${item.author.name}</h4>
+              <h4>${item.author.name ? item.author.name : 'No data found'}</h4>
             </div>
 
+            <p><i class="fa-solid fa-eye"></i> ${
+              item.total_view ? item.total_view : 'No data found'
+            }</p>
+
+            
+            <!-- modal button -->
+            <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="modal" data-bs-target="#exampleModalLg">Read more</button>
+
+            <!-- modal starts-->
+            <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="exampleModalLg" tabindex="-1" aria-labelledby="exampleModalLgLabel" aria-modal="true" role="dialog">
+                <div class="modal-dialog modal-lg relative w-auto pointer-events-none">
+                  <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+                    <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+                      <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalLgLabel">
+                        Large modal
+                      </h5>
+                      <button type="button"
+                        class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                        data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body relative p-4">
+                      ...
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <!-- modal ends-->
             
 
-            <p><i class="fa-solid fa-eye"></i> ${item.total_view}</p>
+
+            
+           
           </div>
         </div>
       </div>
+
+      
   `;
+
+    newsElement.classList.add('bg-white');
+    newsElement.classList.add('rounded-3xl');
+    newsElement.classList.add('shadow-lg');
+
     document.getElementById('news-container').appendChild(newsElement);
   });
 };
