ec481ec25a52d007e0728cb09e9482001be34083
Fahim-H <fahimlutful@gmail.com>
2022-09-03 01:12:37 +0600

    complete modal

--- js/app.js
+++ js/app.js
@@ -56,7 +56,7 @@ const displayNewsByCategory = news => {
   newsContainer.textContent = '';
 
   news.forEach(item => {
-    // console.log(item);
+    console.log(item.author.name);
     let itemDetail;
     if (item.details.length > 400) {
       itemDetail = item.details.slice(0, 400);
@@ -71,7 +71,7 @@ const displayNewsByCategory = news => {
         <div class="w-1/4">
           <img
             class="object-cover h-full p-5 rounded-3xl"
-            src="${item.image_url}"
+            src="${item.thumbnail_url}"
             alt=""
           />
         </div>
@@ -107,16 +107,42 @@ const displayNewsByCategory = news => {
             <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="exampleModalLg" tabindex="-1" aria-labelledby="exampleModalLgLabel" aria-modal="true" role="dialog">
                 <div class="modal-dialog modal-lg relative w-auto pointer-events-none">
                   <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+
+                  <!-- modal header-->
                     <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
                       <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalLgLabel">
-                        Large modal
+                        ${item.title}
                       </h5>
                       <button type="button"
                         class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
                         data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
+
+                    <!-- modal author-->
+                    <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+                      <h5 class="text-sm font-medium leading-normal text-gray-800" id="exampleModalLgLabel">
+                        ${item.author.name} <br> possesses a rating of ${
+      item.rating.number
+    } <br> and is considered  ${item.rating.badge}
+                        <br>
+                        Date: ${item.author.published_date}
+                        
+                      </h5>
+                      
+                      <img class="ml-5 w-[70px] rounded-full" src="${
+                        item.author.img
+                      }"/>
+                      <button type="button"
+                        class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                        data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
                     <div class="modal-body relative p-4">
-                      ...
+                    <img class="mb-10" src="${item.image_url}"/>
+                      <p>${item.details}</p>
+                      
+                      <small class="mt-5">This article has been read ${
+                        item.total_view
+                      } times.</small>
                     </div>
                   </div>
                 </div>
